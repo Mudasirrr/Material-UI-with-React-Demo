73685ea42923b059d8c85965f49d423991ab133c
Mudasirrr <hmudasir714@email.com>
2020-07-05 23:20:51 +0500

    Material-UI Demo

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { Button, Icon, makeStyles, Grid, IconButton, AppBar, Toolbar, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  testButton : {
+    backgroundColor : "red"
+  }
+}));
+
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.testButton}style={{background:"#6200ee"}}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit" className={classes.testButton}>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default App;
